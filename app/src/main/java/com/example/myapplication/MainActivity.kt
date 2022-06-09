1f1e13b890cbba299e5980e3e7f916d9ef5b6ab0
romanknyazev <madaraotsusuki2@gmail.com>
2022-06-10 00:30:51 +0400

    version 0.1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,76 +1,138 @@
 package com.example.myapplication
-
-import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONObject
-import java.lang.Exception
-import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
 
-    val CITY: String = "Ulyanovsk, ru"
-    val API: String ="8e111e527dd99a6d0e69b8a945da397f"
+class MainActivity : AppCompatActivity(), LocationListener {
+    private val locationPermissionCode = 2
+    private var long: Double = 0.0
+    private var lat: Double = 0.0
+    private val API: String ="8e111e527dd99a6d0e69b8a945da397f"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //weatherTask().excute()
+        getLocation()
+        getResult()
+        getAdditionalResult()
+    }
+    private fun getLocation() {
+        var locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        if (location != null) {
+            this.long = location.longitude
+        }
+        if (location != null) {
+            this.lat = location.latitude
+        }
     }
 
-    inner class weatherTask(): AsyncTask<String, Void, String>()
-    {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            findViewById<ProgressBar>(R.id.loader).visibility= View.VISIBLE
-            findViewById<RelativeLayout>(R.id.maimContainer).visibility=View.GONE
-            findViewById<TextView>(R.id.error_text).visibility=View.GONE
+    override fun onLocationChanged(location: Location) {
+        this.long = location.longitude
+        this.long = location.latitude
+        Log.d("Coord", "Coord: ${this.long}latitude ${this.lat}")
+    }
+
+    private fun getResult() {
+        val url = "https://api.openweathermap.org/data/2.5/onecall" +
+                "?lat=${this.lat}" +
+                "&lon=${this.long}" +
+                "&exclude=daily,minutely,hourly,alerts" +
+                "&appid=$API" +
+                "&lang=ru" +
+                "&units=metric"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {
+                response-> SetResult(response, "main")
+            },
+            {
+                Log.d("MyLog", "VolleyError: $it")
+            }
+        )
+        queue.add(stringRequest)
+    }
+
+    private fun getAdditionalResult() {
+        val url = "https://api.openweathermap.org/data/2.5/onecall" +
+                "?lat=${this.lat}" +
+                "&lon=${this.long}" +
+                "&exclude=current,minutely,hourly,alerts" +
+                "&appid=$API" +
+                "&lang=ru" +
+                "&units=metric"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {
+                    response-> SetResult(response, "additional")
+            },
+            {
+                Log.d("MyLog", "VolleyError: $it")
+            }
+        )
+        queue.add(stringRequest)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDateTime(s: String): String? {
+        try {
+            val sdf = SimpleDateFormat("HH:m")
+            val netDate = Date(s.toLong() * 1000)
+            return sdf.format(netDate)
+        } catch (e: Exception) {
+            return e.toString()
         }
+    }
 
-        override fun doInBackground(vararg params: String?): String? {
-            var response:String?
-            try{
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=8e111e527dd99a6d0e69b8a945da397f").readText(Charsets.UTF_8)
+    @SuppressLint("SetTextI18n")
+    private fun SetResult(response: String, type: String) {
+        if (type == "main") {
+            val responseObj = JSONObject(response)
+            val currentWeather = responseObj.getJSONObject("current")
+            var currentWeatherDescriptionArray = currentWeather.getJSONArray("weather")
+            var currentWeatherDescription = currentWeatherDescriptionArray.getJSONObject(0)
+            var sunsetLabel: TextView = findViewById(R.id.sunset_value)
+            var sunriseLabel: TextView = findViewById(R.id.sunrise_value)
+            var uvIndexLabel: TextView = findViewById(R.id.uv_index_value)
+            var pressureLabel: TextView = findViewById(R.id.pressure_value)
+            var windLabel: TextView = findViewById(R.id.wind_value)
+            var statusLabel: TextView = findViewById(R.id.status)
+            sunsetLabel.text = getDateTime((currentWeather.getInt("sunset") + responseObj.getInt("timezone_offset")).toString())
+            sunriseLabel.text = getDateTime((currentWeather.getInt("sunrise") + responseObj.getInt("timezone_offset")).toString())
+            uvIndexLabel.text = currentWeather.getString("uvi")
+            pressureLabel.text = currentWeather.getString("pressure")
+            windLabel.text = currentWeather.getString("wind_speed")
+            statusLabel.text = currentWeatherDescription.getString("main")
+        } else if (type == "additional") {
+            val responseObj = JSONObject(response)
+            val dailyWeatherArray = responseObj.getJSONArray("daily")
+            val dailyWeather = dailyWeatherArray.getJSONObject(0)
+            val dailyTemp = dailyWeather.getJSONObject("temp")
+            var moonPhaseLabel: TextView = findViewById(R.id.moon_phase_value)
+            var tempMinLabel: TextView = findViewById(R.id.temp_min)
+            var tempMaxLabel: TextView = findViewById(R.id.temp_max)
+            tempMinLabel.text = tempMinLabel.text.toString() + "\n" + dailyTemp.getString("min") + "°C"
+            tempMaxLabel.text = tempMaxLabel.text.toString() + "\n" + dailyTemp.getString("max") + "°C"
+            moonPhaseLabel.text = dailyWeather.getString("moon_phase")
 
-            }
-            catch (e:Exception){
-                response = null
-
-            }
-            return response
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            try {
-                val jsonObject = JSONObject(result)
-                val main = jsonObject.getJSONObject("main")
-                val sys = jsonObject.getJSONObject("sys")
-                val wind = jsonObject.getJSONObject("wind")
-                val weather = jsonObject.getJSONObject("weather").getJSONObject(0.toString())
-                val updatedAt:Long = jsonObject.getLong("dt")
-                val updatedAtText = "Updated at:"+SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-                val temp = main.getString("temp")+"°C"
-                val tempMin = "Min temp: "+main.getString("temp_min")+"°C"
-                val tempMax = "Max temp: "+main.getString("temp_max")+"°C"
-                val pressure = main.getString("pressure")
-                val uf = main.getString("uf")
-                val moon = main.getString("moon")
-                val sunrise:Long = sys.getLong("sunrise")
-                val sunset:Long = sys.getLong("sunset")
-                val windSpeed = wind.getString("speed")
-                val humidity = main.getString("humidity")
-                val weatherDescription = weather.getString("description")
-                val address = jsonObject.getString("name")+", "+sys.getString("country")
-
-            } catch (e: NumberFormatException) {
-
-            }
         }
     }
 }
